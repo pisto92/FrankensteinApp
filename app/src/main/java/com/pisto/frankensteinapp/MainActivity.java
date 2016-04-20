f46f9961f3e7fb740da46602dd4d9dbb917cbab5
pisto92 <pisto9228@gmail.com>
2016-04-20 20:41:05 +0200

    aggiunto FAB e addfuel activity

--- app/src/main/java/com/pisto/frankensteinapp/MainActivity.java
+++ app/src/main/java/com/pisto/frankensteinapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.pisto.frankensteinapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
 
 public class MainActivity extends App {
 
@@ -8,5 +11,24 @@ public class MainActivity extends App {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configureFAB();
+    }
+
+    private void launchAddFuelActivity() {
+        Intent launchAddFuelActivity = new Intent(getApplicationContext(), AddFuelActivity.class);
+        startActivity(launchAddFuelActivity);
+    }
+
+    private void configureFAB() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        if (fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    launchAddFuelActivity();
+                }
+            });
+        }
     }
 }
