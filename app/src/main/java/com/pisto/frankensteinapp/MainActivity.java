f0a61636b79a804c14c555347d9dad422e7b32f7
pisto <pisto9228@gmail.com>
2016-04-22 14:47:22 +0200

    add custom toolbar

--- app/src/main/java/com/pisto/frankensteinapp/MainActivity.java
+++ app/src/main/java/com/pisto/frankensteinapp/MainActivity.java
@@ -3,10 +3,15 @@ package com.pisto.frankensteinapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends App
 {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -14,7 +19,30 @@ public class MainActivity extends App
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         configureFAB();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_toolbar_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void launchAddFuelActivity()
