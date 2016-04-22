c4ba5520f7bb05695833957cc268855fb49efc16
pisto <pisto9228@gmail.com>
2016-04-22 14:07:13 +0200

    graph in new line and config

--- app/src/main/java/com/pisto/frankensteinapp/MainActivity.java
+++ app/src/main/java/com/pisto/frankensteinapp/MainActivity.java
@@ -5,27 +5,34 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 
-public class MainActivity extends App {
+public class MainActivity extends App
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         configureFAB();
     }
 
-    private void launchAddFuelActivity() {
+    private void launchAddFuelActivity()
+    {
         Intent launchAddFuelActivity = new Intent(getApplicationContext(), AddFuelActivity.class);
         startActivity(launchAddFuelActivity);
     }
 
-    private void configureFAB() {
+    private void configureFAB()
+    {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        if (fab != null) {
-            fab.setOnClickListener(new View.OnClickListener() {
+        if (fab != null)
+        {
+            fab.setOnClickListener(new View.OnClickListener()
+            {
                 @Override
-                public void onClick(View view) {
+                public void onClick(View view)
+                {
                     launchAddFuelActivity();
                 }
             });
