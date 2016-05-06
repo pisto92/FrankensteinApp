944f811aa85a267db9ce80262f3585421274fa66
pisto <pisto9228@gmail.com>
2016-05-06 18:01:08 +0200

    modificato layout add_fuel_activity

--- app/src/main/java/com/pisto/frankensteinapp/AddFuelActivity.java
+++ app/src/main/java/com/pisto/frankensteinapp/AddFuelActivity.java
@@ -1,13 +1,13 @@
 package com.pisto.frankensteinapp;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
-
-import io.realm.RealmResults;
 
 public class AddFuelActivity extends App
 {
@@ -21,6 +21,8 @@ public class AddFuelActivity extends App
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        final CoordinatorLayout clayout = (CoordinatorLayout) findViewById(R.id.coordinator_layout_add_fuel);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_add_fuel);
         if (fab != null)
         {
@@ -29,29 +31,38 @@ public class AddFuelActivity extends App
                 @Override
                 public void onClick(View view)
                 {
+                    database.beginTransaction();
                     try
                     {
-                        EditText totalkm = (EditText) findViewById(R.id.edittext_total_km);
-                        FuelItem fuel = new FuelItem();
-                        fuel.setTotalKm(Integer.parseInt(totalkm.getText().toString()));
-                        fuel.setPrice(50);
-                        fuel.setLiters(70);
-                        fuel.setLocation("Fornasette");
-                        database.beginTransaction();
+                        FuelItem fuel = createtFuelItemFromField();
                         database.copyToRealm(fuel);
-                        database.commitTransaction();
-
-                        RealmResults<FuelItem> resultsFuel = database.where(FuelItem.class).findAll();
-                        Snackbar.make(view, resultsFuel.size() + " Elementi nel DB", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                        Snackbar.make(clayout, "Aggiunto nuovo pieno.", Snackbar.LENGTH_LONG).setAction("Action", null).show();
 
                     } catch (Exception e)
                     {
-                        database.commitTransaction();
-                        Snackbar.make(view, e.getMessage(), Snackbar.LENGTH_LONG).setAction("Action", null).show();
-
+                        Snackbar.make(clayout, "Errore nell'inserimento dei dati. Controllali!", Snackbar.LENGTH_LONG).setAction("Action", null).show();
                     }
+                    database.commitTransaction();
                 }
             });
         }
+    }
+
+    @NonNull
+    private FuelItem createtFuelItemFromField()
+    {
+
+        EditText totalkm = (EditText) findViewById(R.id.edittext_total_km);
+        EditText price = (EditText) findViewById(R.id.edittext_price);
+        EditText liters = (EditText) findViewById(R.id.edittext_liters);
+        EditText location = (EditText) findViewById(R.id.edittext_location);
+
+        FuelItem fuel = new FuelItem();
+        fuel.setTotalKm(Integer.parseInt(totalkm.getText().toString()));
+        fuel.setPrice(Double.parseDouble(price.getText().toString()));
+        fuel.setLiters(Double.parseDouble(liters.getText().toString()));
+        fuel.setLocation(location.getText().toString());
+
+        return fuel;
     }
 }
